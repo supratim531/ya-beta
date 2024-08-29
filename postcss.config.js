f323e77d5e103d1394d652d92b4b6974ea1ca642
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-08-29 10:45:57 +0530

    supratim531: fix(postcss): Added some PostCSS configuration to support nested declarations

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
     "postcss-import": {},
     "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
-    autoprefixer: {},
+    // autoprefixer: {},
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
   },
 };
